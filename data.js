e75cff1b22421da695ede7f9fc25f67c04877483
Flys1X <ivanov.viacheslav088@gmail.com>
2025-03-11 20:33:21 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,122 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-_",
+      "name": "ТИГР_ИНФ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--2_",
+      "name": "БМП-2М_ИНФ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-_",
+      "name": "ГРАД_ИНФ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "ГЛАВНЫЙ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.39223806536268313,
+          "pitch": 0.017612950241217007,
+          "rotation": 0,
+          "target": "0-_"
+        },
+        {
+          "yaw": 0.1073079194391724,
+          "pitch": 0.020817318936419582,
+          "rotation": 0,
+          "target": "2-_"
+        },
+        {
+          "yaw": 1.0236496148339498,
+          "pitch": 0.051500856456208055,
+          "rotation": 0,
+          "target": "1--2_"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
